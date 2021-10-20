c20212126087c55b03e01c97a69eee20c636f8d1
yastrebow <yastrebow@gmail.com>
2021-10-20 13:38:14 +0300

    Initial commit

---
+++ src/main/java/ru/yastrebov/liquibaseproject/LiquibaseProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.yastrebov.liquibaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseProjectApplication.class, args);
+    }
+
+}
